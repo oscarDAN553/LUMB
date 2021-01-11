f9a7222ee9e8c7845dba2992bafaccc19b5651cf
oscarDAN553 <oscar.del.angel@gmail.com>
2021-01-10 21:13:15 -0600

    registro de usuario .2

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -15,7 +15,7 @@ import (
 func Manejadores() {
 	router := mux.NewRouter()
 
-	router.HandlerFunc("/registro", middlew.ChequeoBD(routers.Registro)).Methods("POST")
+	router.HandleFunc("/registro", middlew.ChequeoBD(routers.Registro)).Methods("POST")
 
 	PORT := os.Getenv("PORT")
 	if PORT == "" {
--- middlew/chequeoDB.go
+++ middlew/chequeoDB.go
@@ -1,4 +1,4 @@
-package meddlew
+package middlew
 
 import (
 	"net/http"
@@ -6,8 +6,8 @@ import (
 	"github.com/oscarDAN553/LUMB/bd"
 )
 
-/*ChequeoBD es el middlew que me pernite conocer el estado de la DB*/
-func ChequeoDB(next http.HandlerFunc) http.HandlerFunc {
+/*ChequeoBD es el middlew que me permite conocer el estado de la DB*/
+func ChequeoBD(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if bd.ChequeoConnection() == 0 {
 			http.Error(w, "Conexion perdida con la DB", 500)
